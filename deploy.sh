7337d1ce68f222dc66683981b07c576e254be4e4
Parithiban <parithiban.g@emishealth.com>
2019-04-19 23:45:32 +0530

    deploy permission

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+set +x
 echo "Github User Name"
 echo $GITHUB_USERNAME
-echo "Github Passwprde"
+echo "Github Password"
 echo $GITHUB_PASSWORD
 echo "Slack Token"
 echo $SLACK_TOKEN
